dc2369f3b8a6202105b275a91ced3994a4fd38d9
kmh <minghaokong77@163.com>
2022-04-16 20:52:48 +0800

    judge: init

---
+++ judge/src/test/java/cn/sdu/judge/JudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sdu.judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
