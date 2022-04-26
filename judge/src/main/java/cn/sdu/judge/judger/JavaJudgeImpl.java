9a3129df1b127e656eaeb02b0a47ca9d55d904e2
kmh <minghaokong77@163.com>
2022-04-26 22:28:31 +0800

    judge: 实现c语言python判题器

--- judge/src/main/java/cn/sdu/judge/judger/JavaJudgeImpl.java
+++ judge/src/main/java/cn/sdu/judge/judger/JavaJudgeImpl.java
@@ -11,6 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
 
+/**
+ * java编译执行器
+ */
 public class JavaJudgeImpl implements JudgeInterface {
     File inputFile;
     File outputFile;
@@ -32,28 +35,27 @@ public class JavaJudgeImpl implements JudgeInterface {
     }
 
     @Override
-    public CompileInfo compile(String input) throws IOException, InterruptedException {
+    public CompileInfo compile(String code) throws IOException, InterruptedException {
         CompileInfo compileInfo = new CompileInfo();
 
         FileOutputStream outputStream = new FileOutputStream(inputFile);
-        outputStream.write(("" + input).getBytes(StandardCharsets.UTF_8));
+        outputStream.write(("" + code).getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
         outputStream.close();
 
         //构建进程，重定向编译输出和错误
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("javac", inputFile.getAbsolutePath(), "-d", compiledFile.getParent(),
-                "-encoding", "UTF-8");
-        processBuilder.redirectError(errorFile);
-        processBuilder.redirectOutput(outputFile);
+                "-encoding", "UTF-8", "-J-Dfile.encoding=UTF-8");
         Process process = processBuilder.start();
-
-        boolean b = errorFile.delete();
         //构建编译信息
         compileInfo.setExitCode(process.waitFor());
         compileInfo.setSuccess(compileInfo.getExitCode() == 0);
-        compileInfo.setError(FileUtil.readAllFromFile(errorFile));
-        compileInfo.setOutput(FileUtil.readAllFromFile(outputFile));
+        compileInfo.setOutput(new String(process.getInputStream().readAllBytes(), StandardCharsets.UTF_8));
+        compileInfo.setError(new String(process.getErrorStream().readAllBytes(), StandardCharsets.UTF_8));
+        //关掉输出流防止后面的读取收到影响
+        process.getErrorStream().close();
+        process.getInputStream().close();
         return compileInfo;
     }
 
@@ -64,16 +66,19 @@ public class JavaJudgeImpl implements JudgeInterface {
         try {
             //构建进程，重定向输入输出和错误
             ProcessBuilder processBuilder = new ProcessBuilder();
-            processBuilder.command("java", "Main");
+            processBuilder.command("java", "-Dfile.encoding=UTF-8", "-classpath", compiledFile.getParent(), "Main");
             processBuilder.redirectInput(checkpoint.getInput().getAbsoluteFile());
             processBuilder.redirectOutput(outputFile);
             processBuilder.redirectError(errorFile);
             Process process = processBuilder.start();
             //构建运行信息
             runInfo.setExitCode(process.waitFor());
+            //关掉输出流防止后面的读取收到影响
+            process.getErrorStream().close();
+            process.getInputStream().close();
             runInfo.setSuccess(FileUtil.fileSame(checkpoint.getOutput(), outputFile));
-            runInfo.setError(FileUtil.readAllFromFile(errorFile));
-            runInfo.setOutput(FileUtil.readAllFromFile(outputFile));
+            runInfo.setError(FileUtil.topLinesFromFile(errorFile, 100));
+            runInfo.setOutput(FileUtil.topLinesFromFile(outputFile, 100));
         } catch (IOException e) {
             runInfo.setSuccess(false);
             runInfo.setError(e.getMessage());
