f5198d3920c20a42bc968fd8af0cda138fdda9f6
jingtao <cjt18338538827@163.com>
2022-05-01 20:15:37 +0800

    支持插入数据库的默认值

--- server/src/main/java/cn/sdu/oj/controller/ProblemController.java
+++ server/src/main/java/cn/sdu/oj/controller/ProblemController.java
@@ -5,14 +5,10 @@ import cn.sdu.oj.entity.ResultEntity;
 import cn.sdu.oj.service.ProblemService;
 import cn.sdu.oj.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.security.MessageDigest;
 
 @RestController
 @RequestMapping("/problem")
